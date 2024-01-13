494213f4f81a0da3947352d653136fc7245c790c
秋枫雁飞 <1714255949@qq.com>
2024-01-13 14:03:51 +0800

    新增reload，content和time参数控制offcanvas组件success和error消息

--- Controller/Offcanvas.php
+++ Controller/Offcanvas.php
@@ -9,6 +9,8 @@ class Offcanvas extends FrontendController
     public function success(string $msg = '处理成功！', mixed $data = '程序已正常处理所有流程！', int $code = 200): string
     {
         $this->assign('msg', $this->request->getParam('msg') ?? $msg);
+        $this->assign('reload', $this->request->getParam('reload') ?? 1);
+        $this->assign('time', $this->request->getParam('time') ?? 3);
         $this->assign('content', $this->request->getParam('content') ?? $data);
         return $this->fetch();
     }
@@ -16,6 +18,8 @@ class Offcanvas extends FrontendController
     public function error(string $msg = '处理失败！', mixed $data = '程序出现了问题，未能成功执行所有流程！', int $code = 400): string
     {
         $this->assign('msg', $this->request->getParam('msg') ?? $msg);
+        $this->assign('reload', $this->request->getParam('reload') ?? 0);
+        $this->assign('time', $this->request->getParam('time') ?? 3);
         $this->assign('content', $this->request->getParam('content') ?? $data);
         return $this->fetch();
     }
