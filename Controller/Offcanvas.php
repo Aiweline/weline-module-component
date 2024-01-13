f5a0b8082c95efcbe2b243d4515b1a6301dab108
秋枫雁飞 <1714255949@qq.com>
2024-01-13 13:52:52 +0800

    offcanvas组件成功和失败信息处理

--- Controller/Offcanvas.php
+++ Controller/Offcanvas.php
@@ -6,8 +6,17 @@ use Weline\Framework\App\Controller\FrontendController;
 
 class Offcanvas extends FrontendController
 {
-    function index()
+    public function success(string $msg = '处理成功！', mixed $data = '程序已正常处理所有流程！', int $code = 200): string
     {
+        $this->assign('msg', $this->request->getParam('msg') ?? $msg);
+        $this->assign('content', $this->request->getParam('content') ?? $data);
+        return $this->fetch();
+    }
+
+    public function error(string $msg = '处理失败！', mixed $data = '程序出现了问题，未能成功执行所有流程！', int $code = 400): string
+    {
+        $this->assign('msg', $this->request->getParam('msg') ?? $msg);
+        $this->assign('content', $this->request->getParam('content') ?? $data);
         return $this->fetch();
     }
 }
--- etc/env.php
+++ etc/env.php
@@ -1,4 +1,4 @@
 <?php
 return [
-    'router' => ''
+    'router' => 'component'
 ];
